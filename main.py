ae73a982156af8c1289fa9e71d4a711005724c6b
Ryan Rapini <ryanrapini@gmail.com>
2012-10-26 23:11:32 -0400

    committing a (very) tenative base for our game
    This is a tough call to make and will probably undergo many iterations
    before I get it to a workable state. Trying to leave only the bare
    essentials in for now, will start to flesh out classes and such next

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print('hello world!')
+import sys, pygame
+from pygame.locals import *
+
+pygame.init()
+fpsClock = pygame.time.Clock()
+
+size = width, height = 1024, 768
+speed = [2, 2]
+
+colors = {
+	'black' : (0, 0, 0),
+	'white' : (255, 255, 255),
+	'blue' : (0, 0, 255),
+	'red' : (0, 255, 0)
+}
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Hello world!')
+
+basicFont = pygame.font.Font('resources/alphbeta.ttf', 48)
+
+def main(argv):
+	screen.fill(colors['black'])
+	while 1:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: 
+				pygame.quit()
+				sys.exit()
+			elif event.type == MOUSEMOTION:
+				mousex, mousey = event.pos
+			elif event.type == MOUSEBUTTONDOWN:
+				mousex, mousey = event.pos
+			elif event.type == MOUSEBUTTONUP:
+				mousex, mousey = event.pos
+
+		screen.fill(colors['black'])
+
+		pygame.display.update()
+		fpsClock.tick(30)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
