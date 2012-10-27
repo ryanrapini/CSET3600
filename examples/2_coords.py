623158d89ace76ac1a0fd7e8df240c6ba3538123
Ryan Rapini <ryanrapini@gmail.com>
2012-10-26 22:59:14 -0400

    added example scripts demonstrating the use of pygame

---
+++ examples/2_coords.py
@@ -0,0 +1,64 @@
+import sys, pygame
+from pygame.locals import *
+
+pygame.init()
+fpsClock = pygame.time.Clock()
+
+size = width, height = 1024, 768
+speed = [2, 2]
+
+colors = {
+	'black' : (0, 0, 0),
+	'white' : (255, 255, 255),
+	'blue' : (0, 0, 255),
+	'red' : (0, 255, 0)
+}
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Hello world!')
+
+basicFont = pygame.font.SysFont("Comic Sans", 48)
+
+def printCoords(x,y):
+	text = basicFont.render('X:{0} Y:{1}'.format(x,y), True, colors['white'], colors['blue'])
+	screen.blit(text, (x,y))
+	return True
+
+def main(argv):
+
+	text = basicFont.render('Hello world!', True, colors['white'], colors['blue'])
+	textRect = text.get_rect()
+	textRect.centerx = screen.get_rect().centerx
+	textRect.centery = screen.get_rect().centery
+	
+	screen.fill(colors['black'])
+
+	pygame.draw.rect(screen, colors['red'], (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
+	screen.blit(text, (100,100))
+
+	while 1:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: 
+				pygame.quit()
+				sys.exit()
+			elif event.type == MOUSEMOTION:
+				mousex, mousey = event.pos
+			elif event.type == MOUSEBUTTONDOWN:
+				mousex, mousey = event.pos
+			elif event.type == MOUSEBUTTONUP:
+				mousex, mousey = event.pos
+
+		screen.fill(colors['black'])
+
+		pygame.draw.rect(screen, colors['red'], (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
+		screen.blit(text, textRect)
+
+		printCoords(mousex,mousey)
+
+
+
+		pygame.display.update()
+		fpsClock.tick(30)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
