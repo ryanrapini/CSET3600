af52702bbdd6c143c4310c1c682513670197e11e
Ryan Rapini <ryanrapini@gmail.com>
2012-10-31 21:19:15 -0400

    Created a nice looking title screen

--- main.py
+++ main.py
@@ -2,11 +2,50 @@ import sys, pygame
 from pygame.locals import *
 
 class Ship:
-	"""Generates a single battleship."""
-	def __init__(self):
-		self.size = 0
+	"""
+	Generates a single battleship.
 
-	def set(self, values):
+	Position is calculated by a given x and y coord, a ship size, and an alignment
+	which are then used to calculate the entire array of values for the ship.
+	"""
+	def __init__(self, size, alignment, x, y):
+		self.size = size
+		self.health = self.size
+		self.alignment = alignment
+		self.x = [x]
+		self.y = [y]
+
+		if (self.size > 5 or self.size < 2):
+			return False 
+
+		# Generate our array of ship positions.
+		if (self.alignment == "h"):
+			self.x = range(self.x[0], self.x[0] + self.size)
+
+		if (self.alignment == "v"):
+			self.y = range(self.y[0], self.y[0] + self.size)
+
+		# Do some checks to make sure our positions are sane and don't leave the board
+		# maybe this stuff should be moved elsewhere but for now it's going here
+		if (len(self.x) - 1 >= 20):
+			return False
+
+		if (len(self.y) - 1 >= 20):
+			return False
+
+	def isHit(x,y):
+		if x in self.x:
+			if y in self.y:
+				self.health -= 1
+				return True
+		return False
+
+
+	def isSunk():
+		if (self.health <= 0):
+			return True
+		else:
+			return False
 
 
 def init():
@@ -17,7 +56,8 @@ def init():
 	size = width, height = 800, 600
 	speed = [2, 2]
 
-	colors = {
+	global color
+	color = {
 	'black' : (0, 0, 0),
 	'white' : (255, 255, 255),
 	'blue' : (0, 0, 255),
@@ -28,27 +68,53 @@ def init():
 	pygame.display.set_caption('Battleship')
 	return screen
 
-def menu():
-	mainFont = pygame.font.Font('resources/alphbeta.ttf', 100)
 
+def menu(screen):
+	# moved text up here for easy access.
+	title = 'Battleshit'
+	subtitle = 'By Allyn Cheney, Ryan Rapini, and Edward Verhovitz'
+
+	# set font
+	mainFont = pygame.font.Font('resources/alphbeta.ttf', 100)
+	subFont = pygame.font.Font('resources/alphbeta.ttf', 30)
+
+	# set background
 	background = 'resources/battleship.jpg'
 	background_surface = pygame.image.load(background)
 	screen.blit(background_surface, (0,0))
-	pygame.display.flip() 
 
-	text = mainFont.render('Battleship', False, colors['white'], colors['black'])
+	# draw title background
+	titleSurface = pygame.Surface((800,130))
+	# make background transparent and black
+	titleSurface.fill(color['black'])
+	titleSurface.set_alpha(200)
+	screen.blit(titleSurface, (0,0))
 
-	# Create a rectangle
-	textRect = text.get_rect()
+	# draw title text, centered
+	titleText = mainFont.render(title, False, color['white'])
+	titleRect = titleText.get_rect()
+	titleRect.centerx = 400
+	screen.blit(titleText, titleRect)
 
+	# draw subtitle text
+	subtitleText = subFont.render(subtitle, False, color['white'])
+	titleRect = subtitleText.get_rect()
+	titleRect.centerx = 400
+	titleRect.top = 90
+	screen.blit(subtitleText, titleRect)
 
-	# Blit the text
-	screen.blit(text, textRect)
 
 def main(argv):
 	screen = init()
+
 	gamemode = 0
+
 	while 1:
+		if gamemode == 0:
+			menu(screen)
+		elif gamemode == 1:
+			game(screen)
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT: 
 				pygame.quit()
