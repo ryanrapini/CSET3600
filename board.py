19d1aabc5be02d0ae8f8549ba47e02ac88e010c8
Ryan Rapini <ryanrapini@gmail.com>
2012-11-02 21:18:08 -0400

    Switched indentation to tabs because it bugged me

--- board.py
+++ board.py
@@ -1,30 +1,30 @@
-    class board():
-        gameboard = [[0,0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0,0]]
-        
-        def setpiece(self, piece, row, col):
-                self.gameboard[row][col] = piece
-                
-        def returnpiece(self, row, col):
-                return self.gameboard[row][col]
-            
-        def checkforhitormiss(self, row, col):
-            hold = self.returnpiece(row, col)
-            if hold != 7 or hold != 8:
-                if hold == 0:
-                    self.setpiece(7, row, col)
-                    return 'miss'
-                else:
-                    self.setpiece(8, row, col)
-                    return 'hit'
-            else:
-                return 'Pick another Space'
-                
+class board():
+	gameboard = [[0,0,0,0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0,0,0,0],
+				 [0,0,0,0,0,0,0,0,0,0]]
+	
+	def setpiece(self, piece, row, col):
+			self.gameboard[row][col] = piece
+			
+	def returnpiece(self, row, col):
+			return self.gameboard[row][col]
+		
+	def checkforhitormiss(self, row, col):
+		hold = self.returnpiece(row, col)
+		if hold != 7 or hold != 8:
+			if hold == 0:
+				self.setpiece(7, row, col)
+				return 'miss'
+			else:
+				self.setpiece(8, row, col)
+				return 'hit'
+		else:
+			return 'Pick another Space'
+				
