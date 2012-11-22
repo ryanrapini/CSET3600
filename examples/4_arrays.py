d4e98a548f2e5168f933e039a6a3ab6a1ed00fc3
Ryan Rapini <ryanrapini@gmail.com>
2012-11-21 23:48:02 -0500

    added array example

---
+++ examples/4_arrays.py
@@ -0,0 +1,16 @@
+import pprint
+
+# with for loop
+array = []
+for x in range(0,10):
+	new = []
+	for y in range(0,10):
+		new.append(0)
+	array.append(new)
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(array)
+
+# or even more elegantly with list comprehensions!
+array = [[0 for i in range(10)] for j in range(10)]
+pp.pprint(array)
